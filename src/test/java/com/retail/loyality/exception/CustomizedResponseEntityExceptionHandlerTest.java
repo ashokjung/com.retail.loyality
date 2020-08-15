b85c53b07a51e60b0c7ca7a966d97166e116f4d3
ashokjung <ashokjung@gmail.com>
2020-08-16 00:29:03 +0530

    Updated  Test for exception Class

--- src/test/java/com/retail/loyality/exception/CustomizedResponseEntityExceptionHandlerTest.java
+++ src/test/java/com/retail/loyality/exception/CustomizedResponseEntityExceptionHandlerTest.java
@@ -1,2 +1,35 @@
-package com.retail.loyality.exception;public class CustomizedResponseEntityExceptionHandlerTest {
+package com.retail.loyality.exception;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.Mockito.when;
+
+@RunWith(MockitoJUnitRunner.class)
+public class CustomizedResponseEntityExceptionHandlerTest {
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private CustomizedResponseEntityExceptionHandler customizedResponseEntityExceptionHandler;
+
+    @Before
+    public void setup() {
+
+    }
+
+
+    @Test
+    public void checkUnexpectedExceptionsAreCaughtAndStatusCode500IsReturnedInResponse()  {
+
+
+    }
 }
+
+
--- src/test/java/com/retail/loyality/exception/ServiceExceptionHandlerTest.java
+++ src/test/java/com/retail/loyality/exception/ServiceExceptionHandlerTest.java
@@ -1,2 +1,27 @@
-package com.retail.loyality.exception;public class ServiceExceptionHandlerTest {
+package com.retail.loyality.exception;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServiceExceptionHandlerTest {
+
+    ServiceExceptionHandler serviceExceptionHandler;
+
+    @Before
+    public void setup() {
+        serviceExceptionHandler = new ServiceExceptionHandler("Test", "Test");
+    }
+
+    @Test
+    public void serviceExceptionHandlerTest() {
+        Assert.assertEquals("Test", serviceExceptionHandler.getMessage());
+        Assert.assertEquals("Test", serviceExceptionHandler.getStatus());
+    }
+
 }
