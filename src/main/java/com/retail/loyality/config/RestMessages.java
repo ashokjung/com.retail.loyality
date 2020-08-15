6ea21624eed2911e0cf4407d0eb327621da52fd1
ashokjung <ashokjung@gmail.com>
2020-08-16 00:19:07 +0530

    Added Rest Message

---
+++ src/main/java/com/retail/loyality/config/RestMessages.java
@@ -0,0 +1,21 @@
+package com.retail.loyality.config;
+
+public interface RestMessages {
+
+    public static final String success = "success";
+    public static final String failure = "failure";
+    public static final String createCustomerSuccess = "Customer Updated Successfully";
+    public static final String createCustomerFailure = "Failed to Create Customer";
+    public static final String updateCustomerSuccess = "Customer Updated Successfully";
+    public static final String updateCustomerFailure = "Failed to Update Customer";
+    public static final String addCustomerAddressSuccess = "Customer Address added Successfully";
+    public static final String addCustomerAddressFailure = "Failed to Add Customer address";
+    public static final String updateCustomerAddressSuccess = "Customer Address Updated Successfully";
+    public static final String updateCustomerAddressFailure = "Failed to Add Customer address";
+    public static final String addCustomerContactSuccess = "Customer Contact added Successfully";
+    public static final String addCustomerContactFailure = "Failed to Add Customer Contact";
+    public static final String updateCustomerContactSuccess = "Customer Contact Updated Successfully";
+    public static final String updateCustomerContactFailure = "Failed to Update Customer Contact";
+    public static final String invalidCustomer = "Invalid Customer";
+
+}
---
+++ src/main/java/com/retail/loyality/config/SecurityConfig.java
@@ -0,0 +1,2 @@
+package com.retail.loyality.config;public class SecurityConfig {
+}
