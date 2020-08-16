3550ec8a11cd97d8fada75c15afa06c2aabb276a
ashokjung <ashokjung@gmail.com>
2020-08-16 17:37:03 +0530

    change

--- src/main/java/com/retail/loyality/config/SecurityConfig.java
+++ src/main/java/com/retail/loyality/config/SecurityConfig.java
@@ -1,2 +1,31 @@
-package com.retail.loyality.config;public class SecurityConfig {
+package com.retail.loyality.config;
+
+/*
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.context.ShutdownEndpoint;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(EndpointRequest.to(ShutdownEndpoint.class))
+                .hasRole("*")
+                .requestMatchers(EndpointRequest.toAnyEndpoint())
+                .permitAll()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/**")
+                .authenticated()
+                .and()
+                .httpBasic();
+    }
 }
+*/
--- src/main/java/com/retail/loyality/service/CustomerServiceImpl.java
+++ src/main/java/com/retail/loyality/service/CustomerServiceImpl.java
@@ -13,6 +13,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerServiceImpl implements CustomerService {
 
+
     private static final Logger LOG = LoggerFactory.getLogger(CustomerServiceImpl.class);
 
     @Autowired
