f2dbf6502c39b37efcdad0d0713841820a6385ad
ashokjung <ashokjung@gmail.com>
2020-08-20 23:25:50 +0530

    updated POM dependency

--- src/main/java/com/retail/loyality/Application.java
+++ src/main/java/com/retail/loyality/Application.java
@@ -2,6 +2,7 @@ package com.retail.loyality;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
