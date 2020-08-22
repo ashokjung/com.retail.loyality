43490e7d7d8715cbaa5e2d25818a6b6c3027644e
ashokjung <ashokjung@gmail.com>
2020-08-22 16:41:06 +0530

    Added Test for Sequence

--- src/test/java/com/retail/loyality/vo/DatabaseSequenceTest.java
+++ src/test/java/com/retail/loyality/vo/DatabaseSequenceTest.java
@@ -1,2 +1,30 @@
-package com.retail.loyality.vo;public class DatabaseSequenceTest {
+package com.retail.loyality.vo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DatabaseSequenceTest {
+
+    DatabaseSequence databaseSequence;
+
+    @Before
+    public void setup()
+    {
+        databaseSequence = new DatabaseSequence();
+        databaseSequence.setSeq(123l);
+        databaseSequence.setId("USER_SEQUENCE");
+    }
+
+    @Test
+    public void databaseSequenceTest()
+    {
+        Assert.assertEquals(123l,databaseSequence.getSeq());
+        Assert.assertEquals("USER_SEQUENCE",databaseSequence.getId());
+    }
 }
