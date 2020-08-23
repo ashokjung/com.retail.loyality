3b6ec928da4f98d4471d892b997762a4b5a0fb64
ashokjung <ashokjung@gmail.com>
2020-08-23 20:59:23 +0530

    Added Test for JWTservice

--- src/test/java/com/retail/loyality/security/JwtUserDetailsServiceTest.java
+++ src/test/java/com/retail/loyality/security/JwtUserDetailsServiceTest.java
@@ -1,2 +1,28 @@
-package com.retail.loyality.security;public class JwtUserDetailsServiceTest {
+package com.retail.loyality.security;
+
+import com.retail.loyality.security.service.JwtUserDetailsService;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.assertj.core.api.Assertions.catchThrowable;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class JwtUserDetailsServiceTest {
+
+    @Autowired
+    JwtUserDetailsService jwtUserDetailsService;
+
+    @Test
+    public void loadUserByUsernameWithException(){
+        Throwable thrown = catchThrowable(() ->
+                jwtUserDetailsService.loadUserByUsername("Test"));
+        Assertions.assertThat(thrown)
+                .isInstanceOf(UsernameNotFoundException.class);
+    }
 }
