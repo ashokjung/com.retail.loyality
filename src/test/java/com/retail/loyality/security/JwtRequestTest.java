b907fa2592443da5898dde11b8f4b560951b264f
ashokjung <ashokjung@gmail.com>
2020-08-23 20:59:11 +0530

    Added Test for JWTrequest

--- src/test/java/com/retail/loyality/security/JwtRequestTest.java
+++ src/test/java/com/retail/loyality/security/JwtRequestTest.java
@@ -1,2 +1,29 @@
-package com.retail.loyality.security;public class JwtRequestTest {
+package com.retail.loyality.security;
+
+import com.retail.loyality.security.request.JwtRequest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class JwtRequestTest {
+
+    JwtRequest jwtRequest;
+
+    @Before
+    public void setup()
+    {
+        jwtRequest = new JwtRequest("Test","Test");
+    }
+
+    @Test
+    public void customerAddressTest()
+    {
+        Assert.assertEquals("Test", jwtRequest.getPassword());
+        Assert.assertEquals("Test",jwtRequest.getUsername());
+    }
 }
