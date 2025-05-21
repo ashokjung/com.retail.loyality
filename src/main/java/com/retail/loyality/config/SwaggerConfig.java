32abd0c1794c4ac683a564cef618dfa639051238
ashokjung <ashokjung@google.com>
2025-05-21 11:39:18 +0000

    update the swagger

--- src/main/java/com/retail/loyality/config/SwaggerConfig.java
+++ src/main/java/com/retail/loyality/config/SwaggerConfig.java
@@ -1,20 +1,13 @@
 package com.retail.loyality.config;
 
 
-import com.retail.loyality.models.Customer;
-import com.retail.loyality.models.CustomerAddress;
-import com.retail.loyality.models.CustomerClubcard;
-import com.retail.loyality.models.CustomerContactDetails;
-import com.retail.loyality.response.CustomerResponse;
-import com.retail.loyality.security.request.JwtRequest;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.ApiKey;
-import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -28,29 +21,22 @@ import java.util.Collections;
 @EnableSwagger2
 public class SwaggerConfig {
     @Bean
-    public Docket docketapi() {
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage("com.retail.loyality.controller"))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(apiInfo())
-                .securitySchemes(Arrays.asList(apiKey())).protocols(Collections.singleton("HTTP"));
-                //.ignoredParameterTypes(Customer.class, CustomerAddress.class, CustomerContactDetails.class, CustomerClubcard.class, JwtRequest.class, CustomerResponse.class);
-
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info().title("Customer loyalty API")
+                        .description("API to work with Customer Profile and Transaction Data")
+                        .version("1.0")
+                        .contact(new Contact().name("Ashok Jung Bahadur").email("ashokjung@gmail.com").url("https://github.com/ashokjung"))
+                        .license(new License().name("License of API")
+                                .url("API license URL")));
     }
 
-    private ApiInfo apiInfo() {
-        return new ApiInfoBuilder()
-                .title("Customer loyalty API")
-                .description("API to work with Customer Profile and Transaction Data")
-                .termsOfServiceUrl("http://localhost:8081/swagger-ui.html")
-                .version("1.0")
-                .contact(new Contact("Ashok Jung Bahadur", "https://github.com/ashokjung", "ashokjung@gmail.com"))
-                .build();
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .protocols(Collections.singleton("http"))
+                .forCodeGeneration(true);
     }
 
-    private ApiKey apiKey() {
-        return new ApiKey("jwtToken", "Authorization", "header");
-    }
 }
